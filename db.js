fa50faa0e076929acd6265ddb4856ef265275c10
Arjyesh Mohapatra <206687012+ArjyeshMohapatra@users.noreply.github.com>
2025-11-08 23:55:59 +0530

    fixed db not working

--- db.js
+++ db.js
@@ -11,6 +11,8 @@ const pool = new Pool({
     idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
     connectionTimeoutMillis: 5000, // INCREASED from 2 seconds to 5 seconds
     statement_timeout: 10000, // NEW: Kill queries that run longer than 10 seconds
+    host: 'db.lfhmtefshjxglmgcrvfu.supabase.co',
+    ssl: { rejectUnauthorized: false }
 });
 
 pool.on('error', (err, client) => {
